51570aafa8293c1e775a789b5a5efb02b966c444
Jeremiah Cook <kc9cqj@gmail.com>
2022-04-08 18:04:13 -0400

    quick fix.

--- vector/includes/modules/edit_orders/eo_common_address_format.php
+++ vector/includes/modules/edit_orders/eo_common_address_format.php
@@ -44,7 +44,7 @@
 			<?php
 			$filter = $address_fields['company'];
 			filtered_unit_array($filter); ?>
-			<td><?php echo iems_pull_down_menu('update_' . $address_name . '_suburb', $filtered_units, $address_fields['suburb'], 'id="update_' . $address_name . '_country" style="background-color:yellow", class="selectpicker" form-control  data-live-search="true"');?>
+			<td><?php echo iems_pull_down_menu('update_' . $address_name . '_suburb', $filtered_units, $address_fields['suburb'], 'id="update_' . $address_name . '_suburb" style="background-color:yellow", class="selectpicker" form-control  data-live-search="true"');?>
 			</td>	
         </tr> 
 		

59c80133d58783769ad27a2a7d552768de758542
Jeremiah Cook <jeremiah.cook@indianapolisems.org>
2022-04-10 01:23:22 -0400

    test

--- vector/includes/functions/extra_functions/iems.php
+++ vector/includes/functions/extra_functions/iems.php
@@ -19,7 +19,7 @@
 /**
  * Queries the unit table in the database and returns an array.
  * 
- * @return mixed[]
+ * @return mixed[] An associative array.
  */
 function unit_lookup() {
 	global $db;

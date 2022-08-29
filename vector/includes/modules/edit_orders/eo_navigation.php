1adb771a88f388b2f56d97e193ac94cb285936e4
Jeremiah Cook <jeremiah.cook@indianapolisems.org>
2022-08-29 10:59:33 -0400

    Changing entry_company to update_delivery_suburb.

--- vector/includes/modules/edit_orders/eo_navigation.php
+++ vector/includes/modules/edit_orders/eo_navigation.php
@@ -57,7 +57,7 @@ echo '</form>';?>
 		<br><br>
 	<?php
 	company_lookup(); 
-	echo iems_pull_down_menu('entry_company',$company_array, $cInfo->entry_company, 'id ="company" style="background-color:yellow", form-control class="selectpicker" data-live-search="true"');?>
+	echo iems_pull_down_menu('update_delivery_company',$company_array, $cInfo->entry_company, 'id ="company" style="background-color:yellow", form-control class="selectpicker" data-live-search="true"');?>
 	<button type="button" class="btn btn-primary btn-lrg add">Load Units for this Agency</button>
 	<?php echo iems_pull_down_menu('entry_suburb', '', $cInfo->entry_suburb, 'id ="suburb" style="background-color:yellow", class="selectpicker" form-control  data-live-search="true"');?>
 	<button type="button" class="btn btn-danger btn-lrg remove">Clear the Unit List</button>

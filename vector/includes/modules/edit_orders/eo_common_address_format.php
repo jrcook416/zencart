b05ea8db3e3ab8c5ce4e3e62bcf05b4a2503857d
jrcook416 <kc9cqj@gmail.com>
2022-08-29 10:48:35 -0700

    fixing the unit box width to 100%.

--- vector/includes/modules/edit_orders/eo_common_address_format.php
+++ vector/includes/modules/edit_orders/eo_common_address_format.php
@@ -33,8 +33,9 @@
             <td class="eo-label">
 			<label for="update_<?php echo $address_name; ?>_company">
 			<?php echo ENTRY_CUSTOMER_COMPANY; ?></label>:&nbsp;</td>
+			<td>
             <?php company_lookup();
-			echo iems_pull_down_menu("update_". $address_name ."_company",$company_array, $address_fields->company, "id ='update_'". $address_name ."'_company' style='background-color:yellow', form-control class='selectpicker' data-live-search='true'");?>
+			echo iems_pull_down_menu("update_" . $address_name . "_company",$company_array, $address_fields['company'], 'id ="update_' . $address_name .'_company" style="background-color:yellow", form-control class="selectpicker" data-live-search="true" data-width="100%"');?>
 			</td>
         </tr>
 
@@ -45,7 +46,12 @@
 
         <tr>
             <td class="eo-label"><label for="update_<?php echo $address_name; ?>_suburb"><?php echo ENTRY_CUSTOMER_SUBURB; ?></label>:&nbsp;</td>
-            <td><input class="form-control" name="update_<?php echo $address_name; ?>_suburb" size="45" value="<?php echo zen_output_string_protected($address_fields['suburb']); ?>" <?php echo $max_suburb_length; ?> id="update_<?php echo $address_name; ?>_suburb"></td>
+            <td>
+            <?php 
+            $filter = $address_fields['company'];
+            filtered_unit_array($filter);
+			echo iems_pull_down_menu("update_" . $address_name . "_suburb", $filtered_units, $address_fields['suburb'], 'id ="update_' . $address_name .'_suburb" style="background-color:yellow", form-control class="selectpicker" data-live-search="true" data-width="100%"');?>
+			</td>
         </tr>
 
         <tr>

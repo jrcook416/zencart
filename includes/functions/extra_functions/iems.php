1f632e6bc8c279e4f88cc197a0df4b32d3d64a69
Jeremiah Cook <jeremiah.cook@indianapolisems.org>
2022-08-31 10:35:40 -0400

    Removing an old ID field from the iems_pull_down function.

--- includes/functions/extra_functions/iems.php
+++ includes/functions/extra_functions/iems.php
@@ -126,10 +126,11 @@ function iems_pull_down_menu($name, $values, $default = '', $parameters = '', $r
   }
 
   $field = '<select rel="select"';
-
+/*
   if (strpos($parameters, 'id=') === false) {
     $field .= ' id="select-' . zen_output_string($name) . '"';
   }
+*/
 
   $field .= ' name="' . zen_output_string($name) . '"';
 
--- vector/includes/functions/extra_functions/iems.php
+++ vector/includes/functions/extra_functions/iems.php
@@ -140,10 +140,11 @@ function iems_pull_down_menu($name, $values, $default = '', $parameters = '', $r
   }
 
   $field = '<select rel="select"';
-
+/*
   if (strpos($parameters, 'id=') === false) {
     $field .= ' id="' . zen_output_string($name) . '"';
   }
+  */
 
   $field .= ' name="' . zen_output_string($name) . '"';
 

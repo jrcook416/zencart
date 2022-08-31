0dec8f592d134168e828e8a91fc022dcada01832
Jeremiah Cook <jeremiah.cook@indianapolisems.org>
2022-08-31 11:54:00 -0400

    Removing the legal/copyright from the footer.

--- includes/templates/bootstrap/common/tpl_footer.php
+++ includes/templates/bootstrap/common/tpl_footer.php
@@ -66,7 +66,10 @@ echo zen_display_banner('static', $banner);
 <!--eof-banner #5 display -->
 
 <!--bof- site copyright display -->
-<div id="siteinfoLegal" class="legalCopyright text-center"><?php echo FOOTER_TEXT_BODY; ?></div>
+<!--
+IEMS CUSTOM CODE
+<div id="siteinfoLegal" class="legalCopyright text-center"><?php /*echo FOOTER_TEXT_BODY; */ ?></div>
+-->
 <!--eof- site copyright display -->
 
 

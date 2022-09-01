05e8acbe66096c48bd9441022b8e68283964f2c2
jrcook416 <kc9cqj@gmail.com>
2022-09-01 05:59:28 -0700

    Start of shift 2022-09-01.

--- includes/templates/bootstrap/templates/tpl_modules_opc_address_block.php
+++ includes/templates/bootstrap/templates/tpl_modules_opc_address_block.php
@@ -114,20 +114,15 @@ if (ACCOUNT_COMPANY === 'true') {
 echo $_SESSION['opc']->formatAddressElement($which, 'street_address', $address['street_address'], ENTRY_STREET_ADDRESS, TABLE_ADDRESS_BOOK, 'entry_street_address', ENTRY_STREET_ADDRESS_MIN_LENGTH, ENTRY_STREET_ADDRESS_TEXT) . $clear_both;
 
 if (ACCOUNT_SUBURB === 'true') {
+    ?>
+    <label class="inputLabel"><?php echo ENTRY_SUBURB;?></label>
+    <?php
     $filter = $address['company'];
     $name = "suburb[$which]select";
     $id = "suburb-$which-select";
     filtered_unit_array($filter);
-<<<<<<< HEAD
-<<<<<<< HEAD
     echo iems_pull_down_menu($name, $filtered_units, $address['suburb'], 'id="' . $id . '" form-control data-live-search="true" data-width="100%" title="Nothing selected."');
-        echo $_SESSION['opc']->formatAddressElement($which, 'suburb', $address['suburb'], ENTRY_SUBURB, TABLE_ADDRESS_BOOK, 'entry_suburb', 0, ENTRY_SUBURB_TEXT) . $clear_both;
-=======
-    echo iems_pull_down_menu($name, $filtered_units, $address['suburb'], 'id="' . $id . '" form-control data-live-search="true" data-width="100%" title="Nothing selected."'
->>>>>>> f24bd34b8272668329d9b09260cc3bcf06586b42
-=======
-    echo iems_pull_down_menu($name, $filtered_units, $address['suburb'], 'id="'$id'" form-control data-live-search="true" data-width="100%" title="Nothing selected."'
->>>>>>> 32ced0ade8f6c7b1a62e878a9fe092be8367b96b
+    echo "<br>";
 }
 
 echo $_SESSION['opc']->formatAddressElement($which, 'city', $address['city'], ENTRY_CITY, TABLE_ADDRESS_BOOK, 'entry_city', ENTRY_CITY_MIN_LENGTH, ENTRY_CITY_TEXT) . $clear_both;

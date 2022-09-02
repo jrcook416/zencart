19670fc393844ab6d91439371532b163e76a1671
Jeremiah Cook <jeremiah.cook@indianapolisems.org>
2022-09-02 11:09:43 -0400

    Forgot to add the template file.

--- includes/templates/bootstrap2/templates/tpl_modules_opc_address_block.php
+++ includes/templates/bootstrap2/templates/tpl_modules_opc_address_block.php
@@ -137,16 +137,8 @@ if (ACCOUNT_COMPANY === 'true') {
 echo $_SESSION['opc']->formatAddressElement($which, 'street_address', $address['street_address'], ENTRY_STREET_ADDRESS, TABLE_ADDRESS_BOOK, 'entry_street_address', ENTRY_STREET_ADDRESS_MIN_LENGTH, ENTRY_STREET_ADDRESS_TEXT) . $clear_both;
 
 if (ACCOUNT_SUBURB === 'true') {
-	?>
-    <label class="inputLabel"><?php echo ENTRY_SUBURB;?></label>
-    <?php
-    $filter = $address['company'];
-    filtered_unit_array($filter);
-    $name = "suburb[$which]";
-    $id = "suburb-$which";
-    echo iems_pull_down_menu($name, $filtered_units, $address['suburb'], 'id="' . $id . '" class="form-control" data-live-search="true" data-width="100%" title="Nothing selected."');
-	$clear_both;
-    echo "<br>";
+	echo $_SESSION['opc']->iemsFormatAddressElement($which, 'suburb', $address['suburb'], ENTRY_SUBURB, TABLE_ADDRESS_BOOK, 'entry_suburb', ENTRY_STREET_ADDRESS_MIN_LENGTH, ENTRY_STREET_ADDRESS_TEXT) . $clear_both;
+	
 }
 
 echo $_SESSION['opc']->formatAddressElement($which, 'city', $address['city'], ENTRY_CITY, TABLE_ADDRESS_BOOK, 'entry_city', ENTRY_CITY_MIN_LENGTH, ENTRY_CITY_TEXT) . $clear_both;

5d789b095012b339fab5ec67922214659cd2b732
Jeremiah Cook <jeremiah.cook@indianapolisems.org>
2022-09-08 09:45:08 -0400

    Changing the custom-select class in the Bootstrap observer file.

--- includes/classes/observers/ZcaBootstrapObserver.php
+++ includes/classes/observers/ZcaBootstrapObserver.php
@@ -4,6 +4,27 @@
 //
 // BOOTSTRAP 3.4.0.
 //
+/**
+ * This is a file containing custom functions for the Indianapolis EMS implementation of Zen Cart.
+ *
+ *
+ * Custom functions for Indianapolis EMS are defined in the /vector/includes/functions/extra_functions directory 
+ * and the /includes/functions/extra_functions directory as per the Zen Cart coding standards.  This file should be
+ * copied to each of those directories and maintained within Git version control.
+ * For the sake of argument, this is the /vector/includes/functions/extra_functions version of this file.
+ * As a standard, all code should be documented using phpDoc standards as laid out in the phpDoc manual and the 
+ * IEMS documentation.
+ * Note: Variables existing inside of functions are tagged in the function docBlock where appropriate.  They will not show in the API documentation.
+ * 
+ *
+ * @package		admin
+ * @category	Indianapolis EMS custom code
+ * @link   		https://iemssupply.net
+ * @author    	Jeremiah Cook <jeremiah.cook@indianapolisems.org>
+ * @copyright 	Copyright (c)2013-2022, Jeremiah Cook <jeremiah.cook@indianapolisems.org>
+ * @license   	https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html GNU General Public License 2
+ * @version 	Jeremiah Cook 2022-04-11, modified for ZC v1.5.7d
+ */
 class ZcaBootstrapObserver extends base
 {
     // -----
@@ -203,9 +224,9 @@ class ZcaBootstrapObserver extends base
             case 'NOTIFY_ZEN_DRAW_PULL_DOWN_MENU':
                 $field = $p2;
                 if (strpos($field, 'class="') !== false) {
-                    $field = str_replace('class="', 'class="custom-select ', $field);
+                    $field = str_replace('class="', 'class="select ', $field);
                 } else {
-                    $field = str_replace('<select ', '<select class="custom-select" ', $field);
+                    $field = str_replace('<select ', '<select class="select" ', $field);
                 }
                 $p2 = $field;
                 break;

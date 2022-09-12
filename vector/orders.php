331e4116b35c8ff6e12cbc39aa4beedf010855c5
Jeremiah Cook <jeremiah.cook@indianapolisems.org>
2022-09-12 09:06:09 -0400

    adjusting some table elements.

--- vector/orders.php
+++ vector/orders.php
@@ -1111,9 +1111,7 @@ foreach ($predefinedCommentsArray as $value) {
               <tr class="dataTableRow">
                 <td class="dataTableContent text-left">
 				  <input type="checkbox" id="checkbox-<?php echo $order->products[$i];?>" name="checkbox-<?php echo $order->products[$i];?>" value="checkbox-<?php echo $order->products[$i];?>">
-				  <?php echo $order->products[$i]['qty']; ?>x
-                </td>
-                <td class="dataTableContent">
+				  x<?php echo $order->products[$i]['qty']; ?>
                 <?php
                     echo $order->products[$i]['name'];
                     if (isset($order->products[$i]['attributes']) && (sizeof($order->products[$i]['attributes']) > 0)) {

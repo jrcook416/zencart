f23ae6a549eed598d8cb22b28718da4881bbe69f
Jeremiah Cook <jeremiah.cook@indianapolisems.org>
2022-09-13 09:42:48 -0400

    putting in changes to the file.

--- includes/templates/bootstrap/templates/tpl_modules_common_address_format.php
+++ includes/templates/bootstrap/templates/tpl_modules_common_address_format.php
@@ -92,10 +92,8 @@ if (ACCOUNT_SUBURB === 'true') {
 	<?php
     $filter = $address['entry_company'];
     filtered_unit_array($filter);?>
-    <label class="inputLabel" for="suburb"><?php echo ENTRY_SUBURB; ?></label>
     <?php
-    echo iems_pull_down_menu($name, $filtered_units, $entry->fields['entry_suburb'], 'id="' . $id . '" form-control data-live-search="true" data-width="100%" title="Nothing selected."');
-    echo $clear_both;?>
+	echo iems_pull_down_menu('entry_suburb', '', $cInfo->entry_suburb, 'id ="entry_suburb", class="selectpicker" form-control  data-live-search="true"');?>
 <div class="p-2"></div>
 <?php
 }

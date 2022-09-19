d3c3661ceade6e1ffa9a7513d2d93e844e0abbea
Jeremiah Cook <jeremiah.cook@indianapolisems.org>
2022-09-19 15:20:11 -0400

    customers.php

--- vector/customers.php
+++ vector/customers.php
@@ -712,6 +712,7 @@ if (zen_not_null($action)) {
 			<div class="row formAreaTitle"><?php echo CATEGORY_COMPANY; ?></div>
 				<div class="formArea">
 					<div class="form-group" id="county">
+						<?php echo zen_draw_label(ENTRY_COMPANY, 'customers_email_address', 'class="col-sm-3 control-label"'); ?>
 						<div class="col-sm-9 col-md-6">
 							<?php echo zen_draw_label(ENTRY_COMPANY, 'customers_email_address', 'class="col-sm-3 control-label"'); ?>
 							<?php 
@@ -722,8 +723,8 @@ if (zen_not_null($action)) {
 						</div>
 					</div>
 					<div class="form-group" id="company">
+						<?php echo zen_draw_label(ENTRY_COMPANY, 'customers_email_address', 'class="col-sm-3 control-label"'); ?>
 						<div class="col-sm-9 col-md-6">
-							<?php echo zen_draw_label(ENTRY_COMPANY, 'customers_email_address', 'class="col-sm-3 control-label"'); ?>
 							<?php
 							if ($error == true) {
 								if ($entry_company_error == true) {

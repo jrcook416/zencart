7f3fcc5cd02f72e060a700787f268780108a7e6c
Jeremiah Cook <jeremiah.cook@indianapolisems.org>
2022-09-19 16:18:54 -0400

    more code.

--- vector/includes/javascript/customers_iems.php
+++ vector/includes/javascript/customers_iems.php
@@ -24,10 +24,10 @@ $(document).ready(function() {
 $(document).on('click', '.addAgency', function(){
 	<?php unit_lookup();?>
 	var data = <?php echo json_encode($unit_array, JSON_UNESCAPED_SLASHES); ?>;
-	var theCounty = $("#entry_county option:selected").text();
-	console.log(theCounty);
+	var lookup = $("#entry_county option:selected").county();
+	console.log(lookup);
 	alert("County logged.");
-	var agFilter = data.filter((data) => data.county === theCounty);
+	var agFilter = data.filter((data) => data.county === lookup);
 	alert("We have loaded the selected county's agencies into the selector."));
 	console.log(data);
 		var html = '';

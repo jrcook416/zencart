0502c88db64fe869e17508b1dd95d5f909c6a8d7
Jeremiah Cook <jeremiah.cook@indianapolisems.org>
2022-09-19 14:47:17 -0400

    adding changes on customers.php.

--- vector/customers.php
+++ vector/customers.php
@@ -712,6 +712,11 @@ if (zen_not_null($action)) {
 			<div class="row formAreaTitle"><?php echo CATEGORY_COMPANY; ?></div>
 				<div class="formArea">
 					<div class="form-group">
+						<?php 
+						county_lookup();
+						echo iems_pull_down_menu('entry_county',$county_array, '', 'id ="entry_company" style="background-color:yellow", form-control class="selectpicker" data-live-search="true"');
+								?>
+								<button type="button" class="btn btn-primary btn-lrg add">Load Units for this County</button>
 						<?php echo zen_draw_label(ENTRY_COMPANY, 'customers_email_address', 'class="col-sm-3 control-label"'); ?>
 						<div class="col-sm-9 col-md-6">
 						<?php

37ff8493d62c749ba40fddc5c498b7b261d79913
Jeremiah Cook <jeremiah.cook@indianapolisems.org>
2022-09-19 11:04:52 -0400

    More changes to the common address block in create_account.php (2).

--- includes/templates/bootstrap/templates/tpl_modules_create_account.php
+++ includes/templates/bootstrap/templates/tpl_modules_create_account.php
@@ -84,7 +84,7 @@ if (ACCOUNT_SUBURB === 'true') {
             <label class="inputLabel" for="suburb"><?php echo ENTRY_SUBURB; ?></label>
 				<br>
 				<?php
-				$filter = $entry->fields['entry_company'];
+				$filter = $entry->fields['company'];
 				filtered_unit_array($filter);
 				echo iems_pull_down_menu('suburb', $filtered_units, $entry->fields['entry_suburb'], 'id ="entry_suburb", form-control data-live-search="true" readonly');?>
             <div class="p-2"></div>

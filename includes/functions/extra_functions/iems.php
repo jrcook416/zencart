0e2d88e715ae1ec4e0b4c213b2a59eb579b759b2
Jeremiah Cook <jeremiah.cook@indianapolisems.org>
2022-11-02 12:16:56 -0400

    Adding the IEMS specific files to the v158 branch.

---
+++ includes/functions/extra_functions/iems.php
@@ -0,0 +1,175 @@
+<?php
+/**
+ * This is a file containing custom functions for the Indianapolis EMS implementation of Zen Cart.
+ *
+ *
+ * Custom functions for Indianapolis EMS are defined in the /vector/includes/functions/extra_functions directory 
+ * and the /includes/functions/extra_functions directory as per the Zen Cart coding standards.  This file should be
+ * copied to each of those directories and maintained within Git version control.
+ * All code should be documented using phpDoc standards as laid out in the phpDoc manual and the 
+ * IEMS documentation.
+ * Note: Variables existing inside of functions are tagged in the function docBlock where appropriate.  They will not show in the API documentation.
+ * 
+ *
+ * @package	IEMSCustomFiles
+ * @subpackage	Vector
+ * @category	Indianapolis EMS custom code
+ * @link   	<https://www.iemssupply.net>
+ * @author    	Jeremiah Cook <jeremiah.cook@indianapolisems.org>
+ * @copyright 	Copyright (c)2013-2022, Jeremiah Cook <jeremiah.cook@indianapolisems.org>
+ * @license   	<https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html> GNU General Public License 2
+ * @version 	Jeremiah Cook 2022-09-20, modified for ZC v1.5.7d
+ */
+/**
+ * Queries the unit table in the database and returns an array of units.
+ *
+ * Note: Variables existing inside of functions are tagged in the function docBlock where appropriate.  They will not show in the API documentation.
+ *
+ * @return 	mixed An associative array ($unit_array) holding unit descriptions and unit filters.
+ * @var		array $db - the database specified in /vector/includes/configure.php
+ * @var		array $unit_array - the associative array that we will load the unit list into.
+ * @var		string $unit_values - the string that holds the MySQL query to pull all columns from the `units` table.
+ */
+function unit_lookup() {
+	global $db;
+	global $unit_array;
+
+	$unit_array = array();
+	$unit_values = $db->Execute("select * from `units`");
+		while (!$unit_values->EOF) {
+			$unit_array[] = array('id' => $unit_values->fields['unit'], 'text' => $unit_values->fields['unit'], 'agency_filter' => $unit_values->fields['unit_filter']);
+			$unit_values->MoveNext();
+			}; //end while
+	return $unit_array; 
+	} //end unit_lookup
+
+/**
+ * Queries the unit table in the database and returns an array of units, filtered by the parameter $filter.
+ *
+ * Note: Variables existing inside of functions are tagged in the function docBlock where appropriate.  They will not show in the API documentation.
+ *
+ * @return 	mixed An associative array ($filtered_units) holding unit descriptions and unit filters.
+ * @param  	string $filter - used to pass the selected agency to the function.  
+ * @var		array $db - the database specified in /vector/includes/configure.php
+ * @var		array $filtered_units - the associative array that we will load the unit list into.
+ * @var		string $unit_values - the string that holds the MySQL query to pull all columns from the `units` table, filtered by the parameter $filter.
+ */
+function filtered_unit_array($filter) {
+	global $db;
+	global $filtered_units;
+	global $filter;
+
+	$filtered_units = array();
+	$unit_values = $db->Execute("select unit from `units` where unit_filter LIKE '" .  $filter . "' order by unit");
+
+		while (!$unit_values->EOF) {
+			$filtered_units[] = array('id' => $unit_values->fields['unit'], 'text' => $unit_values->fields['unit']);
+			$unit_values->MoveNext();
+			};
+	return $filtered_units; 
+	} // end_filtered_unit_array
+	
+/**
+ * Queries the unit table in the database and returns an array of distinct agency values.
+ *
+ * Note: Variables existing inside of functions are tagged in the function docBlock where appropriate.  They will not show in the API documentation.
+ *
+ * @return 	mixed An associative array ($company_array) holding distinct entries in the unit table in `unit_filter`.
+ * @var		array $db - the database specified in /vector/includes/configure.php
+ * @var		array $company_array - the associative array that we will load the unit filter (agency) list into.
+ * @var		string $company_values - the string that holds the MySQL query to pull all distinct agency names from the `units` table.
+ */
+function company_lookup() {
+	global $db;
+	global $company_array;
+
+	$company_array = array();
+	$company_values = $db->Execute("select distinct agency from `units` ");
+
+		while (!$company_values->EOF) {
+			$company_array[] = array('id' => $company_values->fields['agency'], 'text' => $company_values->fields['agency']);
+			$company_values->MoveNext();
+			};
+	return $company_array; 
+	} //end company_array
+
+  /**
+ *  Output a form pull down menu
+ *  Pulls values from a passed array, with the indicated option pre-selected
+ * @param string $name name
+ * @param array $values values
+ * @param string $default default value
+ * @param string $parameters parameters
+ * @param boolean $required required
+ * @return string
+ */
+function iems_pull_down_menu($name, $values, $default = '', $parameters = '', $required = false)
+{
+  // -----
+  // Give an observer the opportunity to **totally** override this function's operation.
+  //
+  $field = false;
+  $GLOBALS['zco_notifier']->notify(
+      'NOTIFY_ZEN_DRAW_PULL_DOWN_MENU_OVERRIDE',
+      array(
+        'name' => $name,
+        'values' => $values,
+        'default' => $default,
+        'parameters' => $parameters,
+        'required' => $required,
+      ),
+      $field
+  );
+  if ($field !== false) {
+    return $field;
+  }
+
+  $field = '<select rel="dropdown"';
+/*
+  if (strpos($parameters, 'id=') === false) {
+    $field .= ' id="select-' . zen_output_string($name) . '"';
+  }
+*/
+
+  $field .= ' name="' . zen_output_string($name) . '"';
+
+  if (zen_not_null($parameters)) {
+    $field .= ' ' . $parameters;
+  }
+
+  $field .= '>' . "\n";
+
+  if (empty($default) && isset($GLOBALS[$name]) && is_string($GLOBALS[$name])) {
+    $default = stripslashes($GLOBALS[$name]);
+  }
+
+  foreach ($values as $value) {
+    $field .= '  <option value="' . zen_output_string($value['id']) . '"';
+    if ($default == $value['id']) {
+      $field .= ' selected="selected"';
+    }
+
+    $field .= '>' . zen_output_string($value['text'], array('"' => '&quot;', '\'' => '&#039;', '<' => '&lt;', '>' => '&gt;')) . '</option>' . "\n";
+  }
+  $field .= '</select>' . "\n";
+
+  if ($required == true) {
+     $field .= TEXT_FIELD_REQUIRED;
+   }
+  // -----
+  // Give an observer the chance to make modifications to the just-rendered field.
+  //
+  $GLOBALS['zco_notifier']->notify(
+      'NOTIFY_ZEN_DRAW_PULL_DOWN_MENU',
+      array(
+        'name' => $name,
+        'values' => $values,
+        'default' => $default,
+        'parameters' => $parameters,
+        'required' => $required,
+      ),
+      $field
+  );
+  return $field;
+} // end iems_pull_down_menu()
+?>
---
+++ vector/includes/functions/extra_functions/iems.php
@@ -0,0 +1,167 @@
+<?php
+/**
+ * This is a file containing custom functions for the Indianapolis EMS implementation of Zen Cart.
+ *
+ *
+ * Custom functions for Indianapolis EMS are defined in the /vector/includes/functions/extra_functions directory 
+ * and the /includes/functions/extra_functions directory as per the Zen Cart coding standards.  This file should be
+ * copied to each of those directories and maintained within Git version control.
+ * For the sake of argument, this is the /includes/functions/extra_functions version of this file.
+ * All code should be documented using phpDoc standards as laid out in the phpDoc manual and the 
+ * IEMS documentation.
+ * Note: Variables existing inside of functions are tagged in the function docBlock where appropriate.  They will not show in the API documentation.
+ * 
+ *
+ * @package		IEMSCustomFiles
+ * @subpackage	Vector
+ * @category	Indianapolis EMS custom code
+ * @link   		<https://www.iemssupply.net>
+ * @author    	Jeremiah Cook <jeremiah.cook@indianapolisems.org>
+ * @copyright 	Copyright (c)2013-2022, Jeremiah Cook <jeremiah.cook@indianapolisems.org>
+ * @license   	<https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html> GNU General Public License 2
+ * @version 	Jeremiah Cook 2022-09-20, modified for ZC v1.5.7d
+ */
+
+ function county_lookup() {
+	global $db;
+	global $county_array;
+
+	$county_array = array();
+	$county_values = $db->Execute("select * from master_county");
+    	while (!$county_values->EOF) {
+			$county_array[] = array(
+			    'id' => $county_values->fields['countyID'],
+			    'code' => $county_values->fields['countyCode'],       
+			    'text' => $county_values->fields['countyCode'] . " " . $county_values->fields['countyName']);
+			$county_values->MoveNext();
+			};
+	return $county_array;	
+	} //end county_array
+
+function filtered_agency_lookup($filter) {
+	global $db;
+	global $agency_array;
+	global $agency_filter;
+
+	$agency_array = array();
+	$agency_values = $db->Execute("select * from `master_agency` where masterCountyID LIKE '" .  $agency_filter . "' order by masterAgency");
+	    print_r($agency_array);
+		while (!$agency_values->EOF) {
+			$agency_array[] = array(
+			    'id' => $agency_values->fields['masterAgencyID'], 
+			    'text' => $agency_values->fields['masterAgency'] . " " .
+			        $agency_values['masterAgencyDescription']);
+			$agency_values->MoveNext();
+			};
+	return $agency_array; 
+	} // end_filtered_agency_lookup
+
+function filtered_unit_lookup() {
+	global $db;
+	global $unit_array;
+	global $unit_filter;
+
+	$unit_array = array();
+	$unit_values = $db->Execute("select * from `master_unit` where masterAgencyID LIKE '" . $unit_filter . "' order by masterUnitDescription");
+		while (!$unit_values->EOF) {
+			$unit_array[] = array(	'id' => $unit_values->fields['masterUnitID'],
+									'text' => $unit_values->fields['masterUnitDescription']);
+			$unit_values->MoveNext();
+			}; //end while
+	return $unit_array; 
+	} //end unit_lookup
+
+  /**
+ *  Output a form pull down menu
+ *  Pulls values from a passed array, with the indicated option pre-selected
+ * @param string $name name
+ * @param array $values values
+ * @param string $default default value
+ * @param string $parameters parameters
+ * @param boolean $required required
+ * @return string
+ */
+function iems_pull_down_menu($name, $values, $default = '', $parameters = '', $required = false)
+{
+  // -----
+  // Give an observer the opportunity to **totally** override this function's operation.
+  //
+  $field = false;
+  $GLOBALS['zco_notifier']->notify(
+      'NOTIFY_ZEN_DRAW_PULL_DOWN_MENU_OVERRIDE',
+      array(
+        'name' => $name,
+        'values' => $values,
+        'default' => $default,
+        'parameters' => $parameters,
+        'required' => $required,
+      ),
+      $field
+  );
+  if ($field !== false) {
+    return $field;
+  }
+
+  $field = '<select rel="dropdown"';
+/*
+  if (strpos($parameters, 'id=') === false) {
+    $field .= ' id="select-' . zen_output_string($name) . '"';
+  }
+*/ 
+
+  $field .= ' name="' . zen_output_string($name) . '"';
+
+  if (zen_not_null($parameters)) {
+    $field .= ' ' . $parameters;
+  }
+
+  $field .= '>' . "\n";
+
+  if (empty($default) && isset($GLOBALS[$name]) && is_string($GLOBALS[$name])) {
+    $default = stripslashes($GLOBALS[$name]);
+  }
+
+  foreach ($values as $value) {
+    $field .= '  <option value="' . zen_output_string($value['id']) . '"';
+    if ($default == $value['id']) {
+      $field .= ' selected="selected"';
+    }
+
+    $field .= '>' . zen_output_string($value['text'], array('"' => '&quot;', '\'' => '&#039;', '<' => '&lt;', '>' => '&gt;')) . '</option>' . "\n";
+  }
+  $field .= '</select>' . "\n";
+
+  if ($required == true) {
+     $field .= TEXT_FIELD_REQUIRED;
+   }
+  // -----
+  // Give an observer the chance to make modifications to the just-rendered field.
+  //
+  $GLOBALS['zco_notifier']->notify(
+      'NOTIFY_ZEN_DRAW_PULL_DOWN_MENU',
+      array(
+        'name' => $name,
+        'values' => $values,
+        'default' => $default,
+        'parameters' => $parameters,
+        'required' => $required,
+      ),
+      $field
+  );
+  return $field;
+} // end iems_pull_down_menu()
+
+function uom_lookup() {
+	global $db;
+	global $uom_array;
+	$uom_array = array();
+	$uom_values = $db->Execute("select uom_id, uom from `uom` ");
+
+		while (!$uom_values->EOF) {
+			$uom_array[] = array('id' => $uom_values->fields['uom'], 'text' => $uom_values->fields['uom']);
+			$uom_values->MoveNext();
+			};
+	return $uom_array; 
+	} //end uom_array
+
+?>

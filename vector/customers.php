0a7e2533e00bc986ca838b487ee08a1f9a3ab818
Jeremiah Cook <jeremiah.cook@indianapolisems.org>
2022-11-04 08:22:31 -0400

    Removing IEMS custom code from the customers.php file until it can be rewritten.

--- vector/customers.php
+++ vector/customers.php
@@ -717,6 +717,7 @@ if (zen_not_null($action)) {
                       echo $cInfo->entry_company . zen_draw_hidden_field('entry_company');
                     }
                   } else {
+						/*
                         county_lookup();
                         echo iems_pull_down_menu('entry_county', $county_array, $cInfo->entry_county, 'id ="entry_county" class = "form-control" data-live-search="true"');?>
 						<br><button type="button" class="btn btn-primary btn-lrg addAgency">Load This County's Agencies into the Agency Selector</button>
@@ -725,7 +726,7 @@ if (zen_not_null($action)) {
 						echo iems_pull_down_menu('entry_company', $company_array, $cInfo->entry_company, 'id ="entry_company" class = "form-control" data-live-search="true"');?>
 						<br><button type="button" class="btn btn-primary btn-lrg addSuburb">Load Units for this Agency</button>
 								<button type="button" class="btn btn-danger btn-lrg removeAgency">Clear the Agency List</button>
-				<?php
+				<?php */
                   }
                   ?>
               </div>

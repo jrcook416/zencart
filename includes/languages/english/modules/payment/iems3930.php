3067a7bea58a6ac0eba153a904b515ce6b8f58da
Jeremiah Cook <jeremiah.cook@indianapolisems.org>
2023-05-29 08:46:48 -0700

    fixing module language files.

--- includes/languages/english/modules/payment/iems3930.php
+++
@@ -1,5 +0,0 @@
-<?php
-define('MODULE_PAYMENT_IEMS3930_TEXT_TITLE', 'Indianapolis EMS Warehouse');
-define('MODULE_PAYMENT_IEMS3930_TEXT_DESCRIPTION','There is no charge for this order. Typically used for granting Free Shipping. Required to allow no-charge checkouts. Required for free downloads.');
-define('MODULE_PAYMENT_IEMS3930_TEXT_EMAIL_FOOTER','There is no charge for this order.');
-?>

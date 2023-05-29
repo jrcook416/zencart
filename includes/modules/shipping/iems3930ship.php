fe3a0cd1e99f3c63ff0fd0e913ee6a5286221f03
Jeremiah Cook <jeremiah.cook@indianapolisems.org>
2023-05-29 15:46:07 -0400

    Fixing the IEMS Unit Location shipping module.

--- includes/modules/shipping/iems3930ship.php
+++ includes/modules/shipping/iems3930ship.php
@@ -24,7 +24,7 @@ class iems3930ship extends base {
     public $code;
     /**
      * $description is a soft name for this shipping method
-     * @var string 
+     * @var string
      */
     public $description;
     /**
@@ -47,7 +47,7 @@ class iems3930ship extends base {
      * @var array
      */
     protected $methodsList = [];
-    /** 
+    /**
      * $quotes is an array containing all the quote information for this shipping module
      * @var array
      */
@@ -72,7 +72,7 @@ class iems3930ship extends base {
      * @var string
      */
     public $title;
-    
+
   /**
    * constructor
    *
@@ -80,15 +80,15 @@ class iems3930ship extends base {
    */
   function __construct() {
     $this->code = 'iems3930ship';
-    $this->title = MODULE_SHIPPING_iems3930ship_TEXT_TITLE;
-    $this->description = MODULE_SHIPPING_iems3930ship_TEXT_DESCRIPTION;
-    $this->sort_order = defined('MODULE_SHIPPING_iems3930ship_SORT_ORDER') ? MODULE_SHIPPING_iems3930ship_SORT_ORDER : null;
+    $this->title = MODULE_SHIPPING_IEMS3930SHIP_TEXT_TITLE;
+    $this->description = MODULE_SHIPPING_IEMS3930SHIP_TEXT_DESCRIPTION;
+    $this->sort_order = defined('MODULE_SHIPPING_IEMS3930SHIP_SORT_ORDER') ? MODULE_SHIPPING_IEMS3930SHIP_SORT_ORDER : null;
     if (null === $this->sort_order) return false;
 
     $this->icon = ''; // add image filename here; must be uploaded to the /images/ subdirectory
-    $this->tax_class = MODULE_SHIPPING_iems3930ship_TAX_CLASS;
-    $this->tax_basis = MODULE_SHIPPING_iems3930ship_TAX_BASIS;
-    $this->enabled = (MODULE_SHIPPING_iems3930ship_STATUS == 'True');
+    $this->tax_class = MODULE_SHIPPING_IEMS3930SHIP_TAX_CLASS;
+    $this->tax_basis = MODULE_SHIPPING_IEMS3930SHIP_TAX_BASIS;
+    $this->enabled = (MODULE_SHIPPING_IEMS3930SHIP_STATUS == 'True');
     $this->update_status();
   }
   /**
@@ -99,10 +99,10 @@ class iems3930ship extends base {
     if (!$this->enabled) return;
     if (IS_ADMIN_FLAG === true) return;
 
-    if (isset($order->delivery) && (int)MODULE_SHIPPING_iems3930ship_ZONE > 0 ) {
+    if (isset($order->delivery) && (int)MODULE_SHIPPING_IEMS3930SHIP_ZONE > 0 ) {
       $check_flag = false;
       $check = $db->Execute("select zone_id from " . TABLE_ZONES_TO_GEO_ZONES . "
-                             where geo_zone_id = '" . MODULE_SHIPPING_iems3930ship_ZONE . "'
+                             where geo_zone_id = '" . MODULE_SHIPPING_IEMS3930SHIP_ZONE . "'
                              and zone_country_id = '" . (int)$order->delivery['country']['id'] . "'
                              order by zone_id");
       while (!$check->EOF) {
@@ -136,21 +136,21 @@ class iems3930ship extends base {
     global $order;
 
     // this code looks to see if there's a language-specific translation for the available shipping locations/methods, to override what is entered in the Admin (since the admin setting is in the default language)
-    $ways_translated = (defined('MODULE_SHIPPING_iems3930ship_MULTIPLE_WAYS')) ? trim(MODULE_SHIPPING_iems3930ship_MULTIPLE_WAYS) : '';
-    $ways_default = trim(MODULE_SHIPPING_iems3930ship_LOCATIONS_LIST);
+    $ways_translated = (defined('MODULE_SHIPPING_IEMS3930SHIP_MULTIPLE_WAYS')) ? trim(MODULE_SHIPPING_IEMS3930SHIP_MULTIPLE_WAYS) : '';
+    $ways_default = trim(MODULE_SHIPPING_IEMS3930SHIP_LOCATIONS_LIST);
     $methodsToParse = ($ways_translated == '') ? $ways_default : $ways_translated;
 
     if ($methodsToParse == '') {
       $this->methodsList[] = array('id' => $this->code,
-                                   'title' => trim((string)MODULE_SHIPPING_iems3930ship_TEXT_WAY),
-                                   'cost' => MODULE_SHIPPING_iems3930ship_COST);
+                                   'title' => trim((string)MODULE_SHIPPING_IEMS3930SHIP_TEXT_WAY),
+                                   'cost' => MODULE_SHIPPING_IEMS3930SHIP_COST);
     } else {
       $this->locations = explode(';', (string)$methodsToParse);
       $this->methodsList = array();
       foreach ($this->locations as $key => $val)
       {
         if ($method != '' && $method != $this->code . (string)$key) continue;
-        $cost = MODULE_SHIPPING_iems3930ship_COST;
+        $cost = MODULE_SHIPPING_IEMS3930SHIP_COST;
         $title = $val;
         if (strstr($val, ',')) {
           list($title, $cost) = explode(',', $val);
@@ -162,7 +162,7 @@ class iems3930ship extends base {
     }
 
     $this->quotes = array('id' => $this->code,
-                          'module' => MODULE_SHIPPING_iems3930ship_TEXT_TITLE,
+                          'module' => MODULE_SHIPPING_IEMS3930SHIP_TEXT_TITLE,
                           'methods' => $this->methodsList);
 
     if ($this->tax_class > 0) {
@@ -181,10 +181,10 @@ class iems3930ship extends base {
   function check() {
     global $db;
     if (!isset($this->_check)) {
-      $check_query = $db->Execute("select configuration_value from " . TABLE_CONFIGURATION . " where configuration_key = 'MODULE_SHIPPING_iems3930ship_STATUS'");
+      $check_query = $db->Execute("select configuration_value from " . TABLE_CONFIGURATION . " where configuration_key = 'MODULE_SHIPPING_IEMS3930SHIP_STATUS'");
       $this->_check = $check_query->RecordCount();
     }
-    if ($this->_check > 0 && !defined('MODULE_SHIPPING_iems3930ship_LOCATIONS_LIST')) $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Pickup Locations', 'MODULE_SHIPPING_iems3930ship_LOCATIONS_LIST', 'Walk In', 'Enter a list of locations, separated by semicolons (;).<br>Optionally you may specify a fee/surcharge for each location by adding a comma and an amount. If no amount is specified, then the generic Shipping Cost amount from the next setting will be applied.<br><br>Examples:<br>121 Main Street;20 Church Street<br>Sunnyside,4.00;Lee Park,5.00;High Street,0.00<br>Dallas;Tulsa,5.00;Phoenix,0.00<br>For multilanguage use, see the define-statement in the language file for this module.', '6', '0', now())");
+    if ($this->_check > 0 && !defined('MODULE_SHIPPING_IEMS3930SHIP_LOCATIONS_LIST')) $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Pickup Locations', 'MODULE_SHIPPING_IEMS3930SHIP_LOCATIONS_LIST', 'Walk In', 'Enter a list of locations, separated by semicolons (;).<br>Optionally you may specify a fee/surcharge for each location by adding a comma and an amount. If no amount is specified, then the generic Shipping Cost amount from the next setting will be applied.<br><br>Examples:<br>121 Main Street;20 Church Street<br>Sunnyside,4.00;Lee Park,5.00;High Street,0.00<br>Dallas;Tulsa,5.00;Phoenix,0.00<br>For multilanguage use, see the define-statement in the language file for this module.', '6', '0', now())");
     return $this->_check;
   }
   /**
@@ -193,13 +193,13 @@ class iems3930ship extends base {
    */
   function install() {
     global $db;
-    $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Enable Store Pickup Shipping', 'MODULE_SHIPPING_iems3930ship_STATUS', 'True', 'Do you want to offer In Store rate shipping?', '6', '0', 'zen_cfg_select_option(array(\'True\', \'False\'), ', now())");
-    $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Pickup Locations', 'MODULE_SHIPPING_iems3930ship_LOCATIONS_LIST', 'Walk In', 'Enter a list of locations, separated by semicolons (;).<br>Optionally you may specify a fee/surcharge for each location by adding a comma and an amount. If no amount is specified, then the generic Shipping Cost amount from the next setting will be applied.<br><br>Examples:<br>121 Main Street;20 Church Street<br>Sunnyside,4.00;Lee Park,5.00;High Street,0.00<br>Dallas;Tulsa,5.00;Phoenix,0.00<br>For multilanguage use, see the define-statement in the language file for this module.', '6', '0', now())");
-    $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Shipping Cost', 'MODULE_SHIPPING_iems3930ship_COST', '0.00', 'The shipping cost for all orders using this shipping method.', '6', '0', now())");
-    $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, use_function, set_function, date_added) values ('Tax Class', 'MODULE_SHIPPING_iems3930ship_TAX_CLASS', '0', 'Use the following tax class on the shipping fee.', '6', '0', 'zen_get_tax_class_title', 'zen_cfg_pull_down_tax_classes(', now())");
-    $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Tax Basis', 'MODULE_SHIPPING_iems3930ship_TAX_BASIS', 'Shipping', 'On what basis is Shipping Tax calculated. Options are<br>Shipping - Based on Store Pickup Address <br>Billing - Based on customers Billing address', '6', '0', 'zen_cfg_select_option(array(\'Shipping\', \'Billing\'), ', now())");
-    $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, use_function, set_function, date_added) values ('Shipping Zone', 'MODULE_SHIPPING_iems3930ship_ZONE', '0', 'If a zone is selected, only enable this shipping method for that zone.', '6', '0', 'zen_get_zone_class_title', 'zen_cfg_pull_down_zone_classes(', now())");
-    $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Sort Order', 'MODULE_SHIPPING_iems3930ship_SORT_ORDER', '0', 'Sort order of display.', '6', '0', now())");
+    $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Enable Store Pickup Shipping', 'MODULE_SHIPPING_IEMS3930SHIP_STATUS', 'True', 'Do you want to offer In Store rate shipping?', '6', '0', 'zen_cfg_select_option(array(\'True\', \'False\'), ', now())");
+    $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Pickup Locations', 'MODULE_SHIPPING_IEMS3930SHIP_LOCATIONS_LIST', 'Walk In', 'Enter a list of locations, separated by semicolons (;).<br>Optionally you may specify a fee/surcharge for each location by adding a comma and an amount. If no amount is specified, then the generic Shipping Cost amount from the next setting will be applied.<br><br>Examples:<br>121 Main Street;20 Church Street<br>Sunnyside,4.00;Lee Park,5.00;High Street,0.00<br>Dallas;Tulsa,5.00;Phoenix,0.00<br>For multilanguage use, see the define-statement in the language file for this module.', '6', '0', now())");
+    $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Shipping Cost', 'MODULE_SHIPPING_IEMS3930SHIP_COST', '0.00', 'The shipping cost for all orders using this shipping method.', '6', '0', now())");
+    $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, use_function, set_function, date_added) values ('Tax Class', 'MODULE_SHIPPING_IEMS3930SHIP_TAX_CLASS', '0', 'Use the following tax class on the shipping fee.', '6', '0', 'zen_get_tax_class_title', 'zen_cfg_pull_down_tax_classes(', now())");
+    $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Tax Basis', 'MODULE_SHIPPING_IEMS3930SHIP_TAX_BASIS', 'Shipping', 'On what basis is Shipping Tax calculated. Options are<br>Shipping - Based on Store Pickup Address <br>Billing - Based on customers Billing address', '6', '0', 'zen_cfg_select_option(array(\'Shipping\', \'Billing\'), ', now())");
+    $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, use_function, set_function, date_added) values ('Shipping Zone', 'MODULE_SHIPPING_IEMS3930SHIP_ZONE', '0', 'If a zone is selected, only enable this shipping method for that zone.', '6', '0', 'zen_get_zone_class_title', 'zen_cfg_pull_down_zone_classes(', now())");
+    $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Sort Order', 'MODULE_SHIPPING_IEMS3930SHIP_SORT_ORDER', '0', 'Sort order of display.', '6', '0', now())");
   }
   /**
    * Remove the module and all its settings
@@ -207,7 +207,7 @@ class iems3930ship extends base {
    */
   function remove() {
     global $db;
-    $db->Execute("delete from " . TABLE_CONFIGURATION . " where configuration_key like 'MODULE\_SHIPPING\_iems3930ship\_%'");
+    $db->Execute("delete from " . TABLE_CONFIGURATION . " where configuration_key like 'MODULE\_SHIPPING\_IEMS3930SHIP\_%'");
   }
   /**
    * Internal list of configuration keys used for configuration of the module
@@ -215,6 +215,6 @@ class iems3930ship extends base {
    * @return array
    */
   function keys() {
-    return array('MODULE_SHIPPING_iems3930ship_STATUS', 'MODULE_SHIPPING_iems3930ship_LOCATIONS_LIST', 'MODULE_SHIPPING_iems3930ship_COST', 'MODULE_SHIPPING_iems3930ship_TAX_CLASS', 'MODULE_SHIPPING_iems3930ship_TAX_BASIS', 'MODULE_SHIPPING_iems3930ship_ZONE', 'MODULE_SHIPPING_iems3930ship_SORT_ORDER');
+    return array('MODULE_SHIPPING_IEMS3930SHIP_STATUS', 'MODULE_SHIPPING_IEMS3930SHIP_LOCATIONS_LIST', 'MODULE_SHIPPING_IEMS3930SHIP_COST', 'MODULE_SHIPPING_IEMS3930SHIP_TAX_CLASS', 'MODULE_SHIPPING_IEMS3930SHIP_TAX_BASIS', 'MODULE_SHIPPING_IEMS3930SHIP_ZONE', 'MODULE_SHIPPING_IEMS3930SHIP_SORT_ORDER');
   }
 }

616a9f8252f9b1abf994c00b2fa02f2416fe4e0b
Jeremiah Cook <jeremiah.cook@indianapolisems.org>
2023-05-30 08:50:51 -0700

    changing permissions on files; changes to opc address block.

--- includes/templates/bootstrap/templates/tpl_modules_opc_address_block.php
+++ includes/templates/bootstrap/templates/tpl_modules_opc_address_block.php
@@ -106,11 +106,16 @@ if (ACCOUNT_GENDER === 'true') {
 }
 
 
-echo $_SESSION['opc']->formatAddressElement($which, 'firstname', $address['firstname'], ENTRY_FIRST_NAME, TABLE_CUSTOMERS, 'customers_firstname', ENTRY_FIRST_NAME_MIN_LENGTH, ENTRY_FIRST_NAME_TEXT) . $clear_both;
+echo $_SESSION['opc']->formatAddressElement($which, 'firstname', $address['firstname'], ENTRY_FIRST_NAME, 
+TABLE_CUSTOMERS, 'customers_firstname', ENTRY_FIRST_NAME_MIN_LENGTH, ENTRY_FIRST_NAME_TEXT, ' readonly') . 
+$clear_both;
 
-echo $_SESSION['opc']->formatAddressElement($which, 'lastname', $address['lastname'], ENTRY_LAST_NAME, TABLE_CUSTOMERS, 'customers_lastname', ENTRY_LAST_NAME_MIN_LENGTH, ENTRY_LAST_NAME_TEXT) . $clear_both;
+echo $_SESSION['opc']->formatAddressElement($which, 'lastname', $address['lastname'], ENTRY_LAST_NAME, 
+TABLE_CUSTOMERS, 'customers_lastname', ENTRY_LAST_NAME_MIN_LENGTH, ENTRY_LAST_NAME_TEXT, ' readonly') . 
+$clear_both;
 
-echo $_SESSION['opc']->formatAddressElement($which, 'company', $address['company'], ENTRY_COMPANY, TABLE_ADDRESS_BOOK, 'entry_company', ENTRY_COMPANY_MIN_LENGTH, ENTRY_COMPANY_TEXT) . $clear_both;
+echo $_SESSION['opc']->formatAddressElement($which, 'company', $address['company'], ENTRY_COMPANY, 
+TABLE_ADDRESS_BOOK, 'entry_company', ENTRY_COMPANY_MIN_LENGTH, ENTRY_COMPANY_TEXT, 'readonly') . $clear_both;
 
 $field_name = "unit[$which]";
 $field_id = "unit-$which";?>
@@ -120,25 +125,29 @@ $field_id = "unit-$which";?>
 $agency = $_SESSION['IEMS']['customer_agency_code'];
 
 filtered_unit_lookup($agency);
-echo iems_pull_down_menu($field_name, $filtered_unit_array, $address['unit'], "id=\"$field_id\"" , false);?>
+echo iems_pull_down_menu($field_name, $filtered_unit_array, $address['unit'], "id=\"$field_id\"");?>
 <br><br>
 <?php
 // END IEMS EDITED CODE //
+
+
 $field_name = "zone_country_id[$which]";
 $field_id = "country-$which";
 ?>
       <label class="inputLabel" for="<?php echo $field_id; ?>"><?php echo ENTRY_COUNTRY; ?></label><br>
       <?php echo zen_get_country_list($field_name, $address['country'], "id=\"$field_id\"") .
-      (zen_not_null(ENTRY_COUNTRY_TEXT) ? '<span class="alert">' . ENTRY_COUNTRY_TEXT . '</span>' : '') . $clear_both; ?>
+      (zen_not_null(ENTRY_COUNTRY_TEXT) ? '<span class="alert">' . ENTRY_COUNTRY_TEXT . '</span>' : '') . 
+$clear_both; ?>
 <?php
 if (ACCOUNT_STATE === 'true') {
     $state_zone_id = "stateZone-$which";
     $zone_field_name = "zone_id[$which]";
 ?>
-      <label class="inputLabel"><?php echo ENTRY_STATE; ?></label>
+      <label class="inputLabel"><?php echo ENTRY_STATE; ?></label><br>
 <?php
     if ($address['show_pulldown_states']) {
-        echo zen_draw_pull_down_menu($zone_field_name, zen_prepare_country_zones_pull_down($address['country']), $address['zone_id'], "id=\"$state_zone_id\"");
+        echo zen_draw_pull_down_menu($zone_field_name, zen_prepare_country_zones_pull_down($address['country']), 
+$address['zone_id'], "id=\"$state_zone_id\"");
         if (zen_not_null(ENTRY_STATE_TEXT)) {
             echo '<span class="alert">' . ENTRY_STATE_TEXT . '</span>';
         }
@@ -146,18 +155,23 @@ if (ACCOUNT_STATE === 'true') {
         echo zen_draw_hidden_field($zone_field_name, $address['zone_name']);
     }
 
-    echo $_SESSION['opc']->formatAddressElement($which, 'state', $address['state'], '', TABLE_ADDRESS_BOOK, 'entry_state', ENTRY_STATE_MIN_LENGTH, ENTRY_STATE_TEXT) . $clear_both;
+    echo $_SESSION['opc']->formatAddressElement($which, 'state', $address['state'], '', TABLE_ADDRESS_BOOK, 
+'entry_state', ENTRY_STATE_MIN_LENGTH, ENTRY_STATE_TEXT, ' readonly') . $clear_both;
 }
-
-echo $_SESSION['opc']->formatAddressElement($which, 'street_address', $address['street_address'], ENTRY_STREET_ADDRESS, TABLE_ADDRESS_BOOK, 'entry_street_address', ENTRY_STREET_ADDRESS_MIN_LENGTH, ENTRY_STREET_ADDRESS_TEXT) . $clear_both;
+echo $_SESSION['opc']->formatAddressElement($which, 'street_address', $address['street_address'], 
+ENTRY_STREET_ADDRESS, TABLE_ADDRESS_BOOK, 'entry_street_address', ENTRY_STREET_ADDRESS_MIN_LENGTH, 
+ENTRY_STREET_ADDRESS_TEXT, ' readonly') . $clear_both;
 
 if (ACCOUNT_SUBURB === 'true') {
     echo $_SESSION['opc']->formatAddressElement($which, 'suburb', $address['suburb'], ENTRY_SUBURB, TABLE_ADDRESS_BOOK, 'entry_suburb', 0, ENTRY_SUBURB_TEXT) . $clear_both;
 }
 
-echo $_SESSION['opc']->formatAddressElement($which, 'city', $address['city'], ENTRY_CITY, TABLE_ADDRESS_BOOK, 'entry_city', ENTRY_CITY_MIN_LENGTH, ENTRY_CITY_TEXT) . $clear_both;
+echo $_SESSION['opc']->formatAddressElement($which, 'city', $address['city'], ENTRY_CITY, TABLE_ADDRESS_BOOK, 
+'entry_city', ENTRY_CITY_MIN_LENGTH, ENTRY_CITY_TEXT, ' readonly') . $clear_both;
 
-echo $_SESSION['opc']->formatAddressElement($which, 'postcode', $address['postcode'], ENTRY_POST_CODE, TABLE_ADDRESS_BOOK, 'entry_postcode', ENTRY_POSTCODE_MIN_LENGTH, ENTRY_POST_CODE_TEXT) . $clear_both;
+echo $_SESSION['opc']->formatAddressElement($which, 'postcode', $address['postcode'], ENTRY_POST_CODE, 
+TABLE_ADDRESS_BOOK, 'entry_postcode', ENTRY_POSTCODE_MIN_LENGTH, ENTRY_POST_CODE_TEXT, ' readonly') . 
+$clear_both;
 ?>
       <div id="messages-<?php echo $which; ?>" class="mt-2"></div>
 </div>

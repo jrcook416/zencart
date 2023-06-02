ceb1bc2b799d034ef9aac12b13239e650206dda9
Jeremiah Cook <jeremiah.cook@indianapolisems.org>
2023-06-02 11:30:35 -0400

    changes to address_block.php.

--- includes/templates/bootstrap/templates/tpl_modules_opc_address_block.php
+++ includes/templates/bootstrap/templates/tpl_modules_opc_address_block.php
@@ -114,9 +114,6 @@ echo $_SESSION['opc']->formatAddressElement($which, 'lastname', $address['lastna
 TABLE_CUSTOMERS, 'customers_lastname', ENTRY_LAST_NAME_MIN_LENGTH, ENTRY_LAST_NAME_TEXT, ' readonly') . 
 $clear_both;
 
-echo $_SESSION['opc']->formatAddressElement($which, 'agency', $address['agency'], ENTRY_AGENCY, 
-TABLE_ADDRESS_BOOK, 'entry_agency', ENTRY_AGENCY_MIN_LENGTH, ENTRY_AGENCY_TEXT, 'readonly') . $clear_both;
-
 $agency = $_SESSION['IEMS']['customer_agency_code'];
 $field_name = "agency[$which]";
 $field_id = "agency-$which";?>

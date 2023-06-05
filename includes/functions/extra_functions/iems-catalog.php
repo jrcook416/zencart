c4bb6d573d93ac9ae3ad776a92c3932ba58c32f3
Jeremiah Cook <jeremiah.cook@indianapolisems.org>
2023-06-05 16:18:45 -0400

    testing again.

--- includes/functions/extra_functions/iems-catalog.php
+++ includes/functions/extra_functions/iems-catalog.php
@@ -44,7 +44,7 @@ function filtered_unit_lookup($agency) {
     return $filtered_unit_array;
 } //end filtered_unit_lookup
 
-    function iems_pull_down_menu($name, $values, $default = '', $parameters = '', $required = true) {
+    function iems_pull_down_menu($name, $values, $default = '', $parameters = '', $required = false) {
       // -----
       // Give an observer the opportunity to **totally** override this function's operation.
       //

0cd43ead5326193891d2386442b30767c0c102c3
Jeremiah Cook <67281291+jrcook416@users.noreply.github.com>
2026-06-27 11:11:10 -0500

    Implement county_lookup function for county data
    Added county_lookup function to retrieve county data from iems_counties table.

--- includes/functions/extra_functions/iems.php
+++ includes/functions/extra_functions/iems.php
@@ -1,17 +1,24 @@
 <?php
+/**
+iems.php
+Custom function definitions for Indianapolis EMS Zencart
+File last edited 2026-06-27, Jeremiah Cook
+**/ 
 
-function test_function(){
-	global $db;
-	echo "This is my test function";
-}
-
+/**
+function `unit_lookup`
+global variables $db, $unit_array
+Returns the $unit_array variable which includes a list of units pulled out of the dataset, commonly used in the create
+account and order placing screens in Zencart. Introduced in older versions of IEMS Zencart.  This code drives all of the 
+pull-down unit selects. CSS styling for this element is specifically noted in the template-specific CSS. 
+**/ 
 
 function unit_lookup() {
 	global $db;
 	global $unit_array;
 
 	$unit_array = array();
-	$unit_values = $db->Execute("select unit_description from `units` order by unit_description");
+	$unit_values = $db->Execute("select unit_description from `iems_units` order by unit_description");
 
 		while (!$unit_values->EOF) {
 			$unit_array[] = array('id' => $unit_values->fields['unit_description'], 'text' => $unit_values->fields['unit_description']);
@@ -20,6 +27,31 @@ function unit_lookup() {
 	return $unit_array; 
 	}
 
+/**
+function county_lookup
+global variables $db, $county_array
+Returns the $county_array variable which includes a list of counties pulled out of the dataset, commonly also used in the create account
+and order placing screens in Zencart.  Introduced in v3.0.0 and based on function unit_lookup above.  This code drives all of the pull-down
+county selects.  CSS styling for this element is specifically noted in the template-specific CSS. 
+**/
+
+function county_lookup() {
+	global $db;
+	global $county_array;
+
+	$county_array = array();
+	$county_values = $db->Execute("select * from `iems_counties` order by iems_county_code");
+
+		while (!$county_values->EOF) {
+			$county_array[] = array(
+                'id' => $unit_values->fields['iems_county_ID'], 
+                'countyCode' => $unit_values->fields['iems_county_code'], 
+                'countyName' => $unit_values->fields['iems_county_name'],
+                'idhsDistrict' => $unit_values->fields['iems_idhs_district'], );
+			$county_values->MoveNext();
+			};
+	return $county_array; 
+	}
 
 function unit_lookup_filter() {
 	global $db;

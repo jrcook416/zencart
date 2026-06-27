c5a4ed6db6bcf615f3ff89d2dacb9d144e6c4441
Jeremiah Cook <67281291+jrcook416@users.noreply.github.com>
2026-06-27 11:29:22 -0500

    Enhance suburb dropdown with label and echo
    Added a label for the suburb dropdown menu and ensured proper echoing of the dropdown menu for suburb.

--- includes/templates/iems/templates/tpl_modules_create_account.php
+++ includes/templates/iems/templates/tpl_modules_create_account.php
@@ -81,7 +81,9 @@ IEMS Edited Code Block -- Beginning
 **/      
 unit_lookup();
 county_lookup();
-echo zen_draw_pull_down_menu('suburb', $unit_array, 'entry_suburb','id="suburb"', 'required'); 
+echo zen_draw_pull_down_menu('suburb', $unit_array, 'entry_suburb','id="suburb"', 'required'); ?>
+<label class="inputLabel" for="suburb"><?php echo ENTRY_SUBURB; ?></label>
+<?php 
 echo zen_draw_pull_down_menu('county', $county_array, 'entry_county', 'id="county"', 'required');
   }
 /**

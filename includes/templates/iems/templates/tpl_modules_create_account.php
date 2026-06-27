abfa9d6f1416f7dec4ef62ed2776e83ba147ab59
Jeremiah Cook <67281291+jrcook416@users.noreply.github.com>
2026-06-26 20:13:19 -0500

    Fix syntax error in suburb dropdown menu

--- includes/templates/iems/templates/tpl_modules_create_account.php
+++ includes/templates/iems/templates/tpl_modules_create_account.php
@@ -76,7 +76,7 @@
 <label class="inputLabel" for="suburb"><?php echo ENTRY_SUBURB; ?></label>
 <?php 
 unit_lookup();
-<?php echo zen_draw_pull_down_menu('suburb', $unit_array, '', 'entry_suburb','id="suburb"'); 
+echo zen_draw_pull_down_menu('suburb', $unit_array, '', 'entry_suburb','id="suburb"'); 
 ?>
 <br class="clearBoth">
 

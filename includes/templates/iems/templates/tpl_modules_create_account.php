46747b689618ef2a932faa379ee97e80d6e92128
Jeremiah Cook <67281291+jrcook416@users.noreply.github.com>
2026-06-27 16:32:11 -0500

    Disable state dropdown menu when applicable

--- includes/templates/iems/templates/tpl_modules_create_account.php
+++ includes/templates/iems/templates/tpl_modules_create_account.php
@@ -154,7 +154,7 @@ END OF MODIFICATION
 ?>
 <label class="inputLabel" for="stateZone" id="zoneLabel"><?php echo ENTRY_STATE; ?></label>
 <?php
-      echo zen_draw_pull_down_menu('zone_id', zen_prepare_country_zones_pull_down($selected_country), $zone_id, 'id="stateZone"');
+      echo zen_draw_pull_down_menu('zone_id', zen_prepare_country_zones_pull_down($selected_country), $zone_id, 'id="stateZone"', 'disabled');
       echo '<span class="alert">' . ((!empty(ENTRY_STATE_TEXT) && (int)zen_config('ENTRY_STATE_MIN_LENGTH') > 0) ? ENTRY_STATE_TEXT : '') . '</span>';
     }
 ?>
